3ad8d642ea83ef46142105066e7172a784f6f128
Gabriele Nunez <gabrielenunez@protonmail.com>
2025-01-20 20:01:23 -0500

    Customization

--- src/app/[countryCode]/(main)/account/@dashboard/integrations/page.tsx
+++ src/app/[countryCode]/(main)/account/@dashboard/integrations/page.tsx
@@ -24,7 +24,7 @@ export default async function Integrations(props: {
   }
 
   return (
-    <div className="w-full" data-testid="addresses-page-wrapper">
+    <div className="w-full" data-testid="integrations-page-wrapper">
       <div className="mb-8 flex flex-col gap-y-4">
         <h1 className="text-2xl-semi">Integrations</h1>
         <p className="text-base-regular">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { getBaseURL } from "@lib/util/env"
+import ProgressBarProvider from "components/providers/ProgressBar"
 import { Metadata } from "next"
 import "styles/globals.css"
 
@@ -8,8 +9,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <html lang="en" data-mode="light">
+    <html lang="en" data-mode="dark" className="dark">
       <body>
+        <ProgressBarProvider></ProgressBarProvider>
         <main className="relative">{props.children}</main>
       </body>
     </html>
