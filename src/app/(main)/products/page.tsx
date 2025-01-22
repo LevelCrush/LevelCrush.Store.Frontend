8485bfb620319e888e711bd474e2320609199a2d
Gabriele Nunez <gabrielenunez@protonmail.com>
2025-01-21 22:24:50 -0500

    Placeholder

--- src/app/(main)/products/page.tsx
+++ src/app/(main)/products/page.tsx
@@ -2,11 +2,12 @@ import { Metadata } from "next";
 
 import FeaturedProducts from "@modules/home/components/featured-products";
 import Hero from "@modules/home/components/hero";
-import { listCollections } from "@lib/data/collections";
+import { getCollectionByHandle, listCollections } from "@lib/data/collections";
 import { getRegion } from "@lib/data/regions";
 import Container from "@levelcrush/elements/container";
 import { YouTubeConfig } from "@levelcrush/config/youtube";
 import { H2 } from "@levelcrush/elements/headings";
+import { notFound } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Products | Level Crush",
@@ -17,11 +18,24 @@ export const metadata: Metadata = {
 export default async function Home(props: {
   params: Promise<{ countryCode: string }>
 }) { */
-export default async function Home() {
+export default async function Products() {
+  const region = await getRegion("us");
+
+  if(!region) { 
+    notFound();
+  }
+
+  const collection = await getCollectionByHandle("members-only");
+
+  if(!collection) {
+    notFound();
+  }
+
   return (
     <>
       <Container minimalCSS={true} className="lg:px-4 mx-auto mt-16">
         <H2>Products here</H2>
+        <FeaturedProducts collections={[collection]} region={region} />
       </Container>
     </>
   );
