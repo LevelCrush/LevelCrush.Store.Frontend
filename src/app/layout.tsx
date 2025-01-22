ec93d96450b93de9be1db8ecfaed4e1289d4be5e
Gabriele Nunez <gabrielenunez@protonmail.com>
2025-01-21 22:35:06 -0500

    Wrapped entire account provider in suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,9 @@ import "styles/globals.css";
 import SiteHeader from "@levelcrush/site_header";
 import { H2 } from "@levelcrush/elements/headings";
 import DiscordLink from "@levelcrush/discord_link";
-import AccountProvider, { AccountProviderContext } from "@levelcrush/account/account_provider";
+import AccountProvider, {
+  AccountProviderContext,
+} from "@levelcrush/account/account_provider";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
@@ -14,25 +16,26 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout(props: { children: React.ReactNode }) {
-
-
-
   if (process.env.FORCE_CONSTRUCTION === "true" || false) {
     return (
       <html lang="en" data-mode="dark" className="dark">
         <body>
-          <AccountProvider>
-            <OffCanvas>
-              <SiteHeader />
-              <Suspense><ProgressBarProvider></ProgressBarProvider></Suspense>
-              <main className="relative min-h-full flex justify-center items-center">
-                <div className="w-full max-w-[50rem] p-4 bg-[rgba(0,0,0,.85)] flex justify-center flex-col items-center">
-                  <H2 className="w-full text-center">We just game.</H2>
-                  <DiscordLink />
-                </div>
-              </main>
-            </OffCanvas>
-          </AccountProvider>
+          <Suspense>
+            <AccountProvider>
+              <OffCanvas>
+                <SiteHeader />
+                <Suspense>
+                  <ProgressBarProvider></ProgressBarProvider>
+                </Suspense>
+                <main className="relative min-h-full flex justify-center items-center">
+                  <div className="w-full max-w-[50rem] p-4 bg-[rgba(0,0,0,.85)] flex justify-center flex-col items-center">
+                    <H2 className="w-full text-center">We just game.</H2>
+                    <DiscordLink />
+                  </div>
+                </main>
+              </OffCanvas>
+            </AccountProvider>
+          </Suspense>
         </body>
       </html>
     );
@@ -43,7 +46,9 @@ export default function RootLayout(props: { children: React.ReactNode }) {
           <AccountProvider>
             <OffCanvas>
               <SiteHeader />
-              <Suspense><ProgressBarProvider></ProgressBarProvider></Suspense>
+              <Suspense>
+                <ProgressBarProvider></ProgressBarProvider>
+              </Suspense>
               <main className="relative">{props.children}</main>
             </OffCanvas>
           </AccountProvider>
