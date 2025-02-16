035d6a919f77b8b672944289215e56cf0da201f3
Primal-13 <primal@levelcrush.com>
2025-02-16 07:24:33 -0500

    Added some debug loggin

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,6 +4,7 @@ import Medusa from "@medusajs/js-sdk";
 let MEDUSA_BACKEND_URL = "http://localhost:9000";
 
 if (process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL) {
+  console.log("Using: ", process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL);
   MEDUSA_BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL;
 }
 
--- src/lib/data/customer.ts
+++ src/lib/data/customer.ts
@@ -112,6 +112,8 @@ export async function login(_currentState: unknown, formData: FormData) {
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
 
+
+
   try {
     await sdk.auth
       .login("customer", "levelcrush-auth", { email, password })
